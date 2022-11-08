d3da8aa3a6443151c6f0b87a53d4f3cce55d6df0
Horatio Caine <horatio.caine@hotmail.co.uk>
2022-11-08 15:41:01 +0900

    Create add, subtract, multiply, divide functions

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    console.log(a + b);
+};
+
+function subtract(a, b) {
+    console.log(a - b);
+};
+
+function multiply(a, b) {
+    console.log(a * b);
+};
+
+function divide(a, b) {
+    console.log(a / b);
+};
+
+add(5, 5);
+subtract(10, 10);
+multiply(15, 15);
+divide(20, 20);
